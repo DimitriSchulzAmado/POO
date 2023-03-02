24efe3c8b4d01649f9b0fd6805e007bc90ea9894
“Dimitri” <dimitri.s.amado@gmail.com>
2023-03-02 19:44:02 -0300

    IF and Else

--- Aula1/src/main/java/org/example/Main.java
+++ Aula1/src/main/java/org/example/Main.java
@@ -1,8 +1,19 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        
+        Scanner in = new Scanner(System.in);
+
+        int burger = in.nextInt();
+        int burger2 = in.nextInt();
+        int burger3 = in.nextInt();
+
+        int sum = burger + burger2 + burger3;
+
+        System.out.println("media burgers: " + (sum/3));
+
+        in.close();
     }
 }
