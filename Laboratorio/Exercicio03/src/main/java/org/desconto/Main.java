205a85993969a7136865409a2480d52654c2e6ac
“Dimitri” <dimitri.s.amado@gmail.com>
2023-03-07 09:33:03 -0300

    Making code funcionality

--- Laboratorio/Exercicio03/src/main/java/org/desconto/Main.java
+++ Laboratorio/Exercicio03/src/main/java/org/desconto/Main.java
@@ -1,33 +1,43 @@
 package org.desconto;
 
+import java.io.IOException;
 import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner input = new Scanner(System.in);
 
-        double qtdLitros, payment;
-        String type; // A = Alcool | G = Gasolina
+        double amountLiters, payGas, payAlcohol;
+        char letter; // A = Alcohol | G = Gasoline
 
-        System.out.println("Qtd de litros vendidos: ");
-        qtdLitros = input.nextDouble();
+        // Input Data (reading a caractere)
+        System.out.println("Fuel letter [A/G]: ");
+        letter = (char)System.in.read();
 
-        System.out.println("Tipo de Combustivel [A/G]: ");
-        type = input.nextLine();
+        System.out.println("How much liters? ");
+        amountLiters = input.nextDouble();
 
-        // Gasolina
-        if(qtdLitros <= 20) {
-            // 0,04 por litro
+        // Calculate and print the amount to be paid knowing that G = (6 reais) and (A = 4.90 reais)
+        // Gasoline
+        if(amountLiters <= 20 && (letter == 'G')) {
+            // 0,04 the liter
+            payGas = amountLiters * 6 - (amountLiters * 6) * 0.04;
+            System.out.println("Total a pagar com 4% de desconto: " + payGas);
+
         }else {
-            //0,06 por litro
+            //0,06 the liter
+            payGas = amountLiters * 6 - (amountLiters * 6) * 0.06;
+            System.out.println("Total a pagar com 6% de desconto: " + payGas);
         }
 
-        // Alcool
-        if (qtdLitros <= 20) {
-            // 0,03 por litro
+        // Alcohol
+        if (amountLiters <= 20 && (letter == 'A')) {
+            // 0,03 the liter
+            payAlcohol = amountLiters * 4.90 - (amountLiters * 4.90) * 0.03;
+            System.out.println("Total a pagar com 3% de desconto: " + payAlcohol);
         }else {
-            // 0,05 por litro
+            // 0,05 the liter
+            payAlcohol = amountLiters * 4.90 - (amountLiters * 4.90) * 0.05;
+            System.out.println("Total a pagar com 3% de desconto: " + payAlcohol);
         }
-
-        // Calcular e imprimir o valor a ser pago sabendo que G = (6 reais) e (A = 4,90 reais)
     }
 }
