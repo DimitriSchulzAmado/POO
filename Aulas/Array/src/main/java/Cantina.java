e13e076dbfe446151c20ef4c52552178a945f4fb
“Dimitri” <dimitri.s.amado@gmail.com>
2023-03-17 21:49:13 -0300

    Delete constructor

--- Aulas/Array/src/main/java/Cantina.java
+++ Aulas/Array/src/main/java/Cantina.java
@@ -3,16 +3,10 @@ public class Cantina {
     Salgado [] salgados = new Salgado[3];
     String nome;
 
-    public Cantina() {
-        this.nome = nome;
-    }
-
     public void mostraInfo(){
-
         for (int i = 0; i < salgados.length; i++) {
             if (salgados[i] != null)
                 System.out.println("O salgado: " + salgados[i].nome);
-
         }
     }
     public void addSalgado(Salgado novoSalgado){
