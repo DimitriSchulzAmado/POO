9c1b965598cc26a2cbe195cd2aaca2acf49bf3bc
“Dimitri” <dimitri.s.amado@gmail.com>
2023-05-28 23:35:42 -0300

    creating Usuario and instantiating together with RedeSocial in main

--- Aulas/PracticalExercise-SocialNetwork/src/Main.java
+++ Aulas/PracticalExercise-SocialNetwork/src/Main.java
@@ -1,5 +1,14 @@
+import main.RedeSocial;
+import main.Usuario;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        // Instanciando todas redes sociais
+        RedeSocial[] redesSociais = new RedeSocial[2];
 
+        // Instanciando Usuario
+        Usuario usuario = new Usuario(redesSociais);
     }
 }
---
+++ Aulas/PracticalExercise-SocialNetwork/src/main/Usuario.java
@@ -0,0 +1,10 @@
+package main;
+
+public class Usuario {
+    private String nome;
+    private String email;
+    public Usuario(RedeSocial[] redesSociais) {
+        this.nome = nome;
+        this.email = email;
+    }
+}
