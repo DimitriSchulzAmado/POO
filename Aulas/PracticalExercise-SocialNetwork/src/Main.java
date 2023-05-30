c66cbdcd0eecffbb903e091c67f12169dd5d695f
“Dimitri” <dimitri.s.amado@gmail.com>
2023-05-29 23:31:21 -0300

    Making main functionality

--- Aulas/PracticalExercise-SocialNetwork/src/Main.java
+++ Aulas/PracticalExercise-SocialNetwork/src/Main.java
@@ -1,14 +1,27 @@
-import main.RedeSocial;
-import main.Usuario;
-
-import java.util.Scanner;
+import controllers.errors.NotFoundSocialMediaException;
+import models.abstracts.RedeSocial;
+import models.Usuario;
+import views.GooglePlus;
+import views.Instagram;
 
 public class Main {
     public static void main(String[] args) {
         // Instanciando todas redes sociais
-        RedeSocial[] redesSociais = new RedeSocial[2];
+        final RedeSocial[] redesSociais = new RedeSocial[2];
+        final RedeSocial redeSocial1 = new Instagram("testando123", 2);
+        final RedeSocial redeSocial2 = new GooglePlus("teste",4);
+
+        redesSociais[0] = redeSocial1;
+        redesSociais[1] = redeSocial2;
 
         // Instanciando Usuario
-        Usuario usuario = new Usuario(redesSociais);
+        final Usuario usuario = new Usuario("Dimitri", "dimitri.schulz@ges.inatel.br", redesSociais);
+
+        // Mostrando informações/ações
+        try {
+            usuario.mostraInfoRedeSocial();
+        } catch (NotFoundSocialMediaException e) {
+            e.printStackTrace();
+        }
     }
 }
