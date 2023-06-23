b4057aa4b9b72164ece716d007c101babe3533a4
“Dimitri” <dimitri.s.amado@gmail.com>
2023-06-23 18:53:00 -0300

    Removing method

--- Aulas/Project/src/model/Product.py
+++ Aulas/Project/src/model/Product.py
@@ -2,15 +2,9 @@
 class Product:
 
     def __int__(self, name, price, brand, quantity):
-
         self.name = name
         self.price = price
         self.brand = brand
         self.quantity = quantity
 
-    def display_info(self):
-        '''show info from product'''
-        print(f"Produto: {self.name}")
-        print(f"Preço: ${self.price}")
-        print(f"Marca: ${self.brand}")
-        print(f"Quantidade: ${self.quantity}")
+
